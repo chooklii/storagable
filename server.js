14eb01d57c2db9698b713eea97fe37d271cd5476
Chooklii <=benjaminzenth@icloud.com>
2020-03-15 16:18:50 +0100

    rollback

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ const fileupload = multer({storage: FileStorage}).array("files[]")
 
 const getDirectories = source =>
   fs.readdirSync(source, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
     .filter(dirent => dirent.name != "NeueDateien")
     .map(dirent => dirent.name)
 

d8732605bbdc13ef3af47441c43f354d5995913e
Benjamin Zenth <benjamin.zenth@bechtle.com>
2020-03-19 15:28:07 +0100

    rm docx

--- src/Photobook.js
+++ src/Photobook.js
@@ -52,7 +52,7 @@ componentDidUpdate(){
         const only_photos_list = []
         for(var i = 0; i < photolist.length; i++){
             const ending = photolist[i].slice(photolist[i].indexOf("."), photolist[i].length)
-            if([".mov", ".m4v", ".mp4"].indexOf(ending) <= 0){
+            if(["docx"].indexOf(ending) <= 0){
                 only_photos_list.push(photolist[i])
             }
         }

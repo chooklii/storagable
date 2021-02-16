e2126fa7d142a8038d73c0fa9813ad06be33a4e5
Benjamin Zenth <benjaminzenth@icloud.com>
2021-02-16 18:54:38 +0100

    fix desktop design upload remove unused bundle update bable

--- src/config_template.js
+++ src/config_template.js
@@ -1,8 +1,8 @@
 const IP_ADRESS = "IP_ADRESS"
-const PORT = "8000"
+const PORT = ":8000"
 const HTTP_METHOD = "http://"
 const FULL_ROUTE = HTTP_METHOD + IP_ADRESS + PORT
-export default {
+export {
     IP_ADRESS,
     PORT,
     HTTP_METHOD,
